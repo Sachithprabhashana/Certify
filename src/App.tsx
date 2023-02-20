7dbd68a6dcad8268d5a815889498526103077444
sachith <sachithprabhashana@gmail.com>
2023-02-20 23:01:58 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.less';
+import { Navbar } from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       certify
     </div>
   );

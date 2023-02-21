cc460fa1455bb818a04550a54894783f84c46957
sachith <sachithprabhashana@gmail.com>
2023-02-21 21:46:50 +0530

    navbar created

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,7 +1,38 @@
-import React,{ FC } from 'react'
+import { Menu } from 'antd'
+import {useNavigate} from 'react-router-dom';
 
-export const Navbar:FC = () => {
+
+
+export const Navbar = ({isInline = false}) => {
+  const navigate = useNavigate()
   return (
-    <div>Navbar</div>
+    <div className='navbar' style={{backgroundColor: '#fefefa',height:'10vh'}}>
+      <Menu
+      onClick={({key})=> {
+        if(key === 'login'){
+
+        }else{
+          navigate(key)
+        }
+      }}
+       mode={isInline ? 'inline':'horizontal'} style={{backgroundColor:'	#EEE8AA',alignItems:'center',justifyContent:'flex-end',fontSize:20,height:'10vh'}} items={[
+        {
+          label:'Home',
+          key:'/'
+        },
+        {
+          label:'Contact Us',
+          key:'/contact'
+        },
+        {
+          label:'About Us',
+          key:'/about'
+        },
+        {
+          label:'Login',
+          key:'/login'
+        }
+      ]}></Menu>
+    </div>
   )
 }

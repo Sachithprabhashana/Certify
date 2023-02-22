cbfb1bbb1e9955d8eb9a794abd11e9dbfb72d666
sachith <sachithprabhashana@gmail.com>
2023-02-22 08:20:45 +0530

    changes

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -7,14 +7,14 @@ export const Home:FC = () => {
     <div style={{display:'flex',justifyContent:'center',alignSelf:'center',paddingTop:'100px'}}>
     <Space direction="vertical">
     <Space wrap>
-      <Tooltip title="single certificates">
-      <Button style={{fontSize:30,width:'300px',height:'100px'}} icon={<UserOutlined />}>Single Player</Button>
+      <Tooltip title="single player certificate">
+      <Button style={{fontSize:30,width:'300px',height:'100px'}} icon={<UserOutlined />}>Single Event</Button>
       </Tooltip>
     </Space>
 
     <Space wrap>
-      <Tooltip title="team certificates">
-      <Button  style={{fontSize:30,width:'300px',height:'100px'}} icon={<TeamOutlined />}>Team Player</Button>
+      <Tooltip title="team player certificates">
+      <Button  style={{fontSize:30,width:'300px',height:'100px'}} icon={<TeamOutlined />}>Team Event</Button>
       </Tooltip>
     </Space>
   </Space>

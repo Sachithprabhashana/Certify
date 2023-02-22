4d022bd153739bf762cf7ae480b218476d6132ce
sachith <sachithprabhashana@gmail.com>
2023-02-22 06:51:35 +0530

    navbar menu

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,7 +1,28 @@
 import React,{ FC } from 'react'
+import { Menu } from 'antd'
 
 export const Navbar:FC = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar' style={{backgroundColor: '#fefefa',height:'10vh'}}>
+    <Menu
+     style={{backgroundColor:'	#EEE8AA',alignItems:'center',justifyContent:'flex-end',fontSize:20,height:'10vh'}} items={[
+      {
+        label:'Home',
+        key:'/'
+      },
+      {
+        label:'Contact Us',
+        key:'/contact'
+      },
+      {
+        label:'About Us',
+        key:'/about'
+      },
+      {
+        label:'Login',
+        key:'/login'
+      }
+    ]}></Menu>
+  </div>
   )
 }

b78cb9e07ce0e37f06a02ed6f762239d80078dee
Kaveendra Perera <kavindratk@gmail.com>
2023-03-06 14:22:47 +0530

    fixes

--- src/pages/SingleEvent/SingleEvent.tsx
+++ src/pages/SingleEvent/SingleEvent.tsx
@@ -51,7 +51,7 @@ export const SingleEvent: FC = () => {
                   style={{
                     fontFamily: 'Shantell Sans',
                     paddingLeft: 140,
-                    paddingTop: 350,
+                    paddingTop: 348,
                     fontStyle: 'italic',
                     fontSize:'20px',
                   }}>

7a4b62bb1468d7bc8d21096ac0d6ac2dd4a9bf8a
sachith <sachithprabhashana@gmail.com>
2024-03-19 12:35:42 +0530

    Update SingleEvent.tsx
    style changes

--- src/pages/SingleEvent/SingleEvent.tsx
+++ src/pages/SingleEvent/SingleEvent.tsx
@@ -51,7 +51,7 @@ export const SingleEvent: FC = () => {
                   style={{
                     fontFamily: 'Shantell Sans',
                     paddingLeft: 140,
-                    paddingTop: 340,
+                    paddingTop: 345,
                     fontStyle: 'italic',
                     fontSize: '20px',
                   }}>
